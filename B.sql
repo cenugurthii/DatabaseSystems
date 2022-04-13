27c70657d1c2a7b0e3ee264b6edd6643f0c5d156
cenugurthii <101983763+cenugurthii@users.noreply.github.com>
2022-04-14 04:12:44 +0500

    Database Systems Table Manipulation

---
+++ B.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO customer VALUES (4,"Perveen","Los Angeles USA","1 155 437 5678",25);
+-- INSERT INTO customer VALUES (5,"Jacky","Los Angeles USA","1 155 437 5678",24);
+-- SELECT * FROM customer;
+
+-- UPDATE employee SET Salary = 850 WHERE Employee_ID = 2;
+-- UPDATE employee SET JobTitle = "Asst. Manager" WHERE Employee_ID = 1;
+-- SELECT * FROM employee;
+
+-- DELETE FROM employee WHERE Employee_ID = 1;
+-- SELECT * FROM employee;
+
+-- SELECT * FROM bill;
+
+-- SELECT * FROM customer JOIN transaction WHERE customer.customer_ID=transaction.customer_ID;
+
+-- SELECT * FROM customer JOIN bill WHERE customer.customer_ID=bill.customer_ID;
+
+-- SELECT Min(Salary),Max(Salary),Avg(Salary) FROM employee;
+
+-- SELECT * FROM employee,services;
